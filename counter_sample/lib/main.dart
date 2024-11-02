a99e9b87ce26df15f5d2e66d9859f2c2d5255e78
Codefarmer <abadasamuelosp@gmail.com>
2024-11-02 13:12:18 +0100

    chore: notifiers

--- counter_sample/lib/main.dart
+++ counter_sample/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,6 +21,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// CounterNotifier with ChangeNotifier functionality
+class CounterNotifier extends ChangeNotifier {
+  int _counter = 0;
+
+  int get counter => _counter;
+
+  void increment() {
+    _counter++;
+    notifyListeners();
+  }
+}
+
+// RestorableCounterNotifier for state restoration
+class RestorableCounterNotifier
+    extends RestorableChangeNotifier<CounterNotifier> {
+  @override
+  CounterNotifier createDefaultValue() => CounterNotifier();
+
+  @override
+  CounterNotifier fromPrimitives(Object? data) {
+    final notifier = CounterNotifier();
+    notifier._counter = data as int;
+    return notifier;
+  }
+
+  @override
+  Object? toPrimitives() => value.counter;
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -32,26 +60,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with RestorationMixin {
-  // State restoration for counter
-  final RestorableInt _counter = RestorableInt(0);
+  late final RestorableCounterNotifier _counterNotifier;
 
   @override
   String? get restorationId => 'my_home_page';
 
   @override
   void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
-    registerForRestoration(_counter, 'counter');
+    registerForRestoration(_counterNotifier, 'counter_notifier');
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter.value++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _counterNotifier = RestorableCounterNotifier();
   }
 
   @override
   void dispose() {
-    _counter.dispose();
+    _counterNotifier.dispose();
     super.dispose();
   }
 
@@ -69,15 +96,20 @@ class _MyHomePageState extends State<MyHomePage> with RestorationMixin {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '${_counter.value}',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ListenableBuilder(
+              listenable: _counterNotifier.value,
+              builder: (context, _) {
+                return Text(
+                  '${_counterNotifier.value.counter}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => _counterNotifier.value.increment(),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
